9ebe300d4a559af4be44c73b09496e29c9dae6a9
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-23 00:05:17 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,7 @@
-@npm run build
-@npm run dev
-@chmod +x khml.js
-@npm install
+```
+$ npm run build
+$ npm run dev
+$ chmod +x khml.js
+$ npm install
+
+$ sha256:e19fa8f2d4f900d848200a5045d36c89aebff0340ff1aa59f08be1856ed90105
