082573d69bb531c1de4320b1efbd0f9884995200
Web4 <167559384+Web4application@users.noreply.github.com>
2025-12-01 21:11:46 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,12 @@
-```
-$ npm run build
-$ npm run dev
-$ chmod +x khml.js
-$ npm install
 
-$ sha256:e19fa8f2d4f900d848200a5045d36c89aebff0340ff1aa59f08be1856ed90105
+npm run build
+npm run dev
+chmod +x khml.js
+npm install
+
+ sha256:e19fa8f2d4f900d848200a5045d36c89aebff0340ff1aa59f08be1856ed90105
+curl \
+    --cert path_to_key_and_cert.pem \
+    --header "content-type: application/json" \
+    --data-binary "@table.json" \
+    https://rst-api-ote.icann.org/v1/table
